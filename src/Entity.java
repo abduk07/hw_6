4ca0778f390e16f861fbc7d4513eed145a9f7b07
variskurbanov <abduk1910@mail.ru>
2023-02-19 20:27:19 +0600

    dodelal

--- src/Entity.java
+++ src/Entity.java
@@ -4,57 +4,30 @@ public class Entity {
     private int health;
     private WeaponType weapon;
 
-    private int heroDamage;
-    private int heroHealth;
-    private WeaponType heroWeapon;
-
-    public int getBossDamage() {
-        return damage;
-    }
-
-    public void setBossDamage(int bossDamage) {
-        this.damage = bossDamage;
-    }
-
-    public int getBossHealth() {
+    public int getHealth() {
         return health;
     }
 
-    public void setBossHealth(int bossHealth) {
-        this.health = bossHealth;
+    public void setHealth(int health) {
+        this.health = health;
     }
 
-    public WeaponType getBossWeapon() {
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public WeaponType getWeapon() {
         return weapon;
     }
 
-    public void setBossWeapon(WeaponType bossWeapon) {
-        this.weapon = bossWeapon;
+    public void setWeapon(WeaponType weapon) {
+        this.weapon = weapon;
     }
 
-    public int getHeroDamage() {
-        return heroDamage;
-    }
-
-    public void setHeroDamage(int heroDamage) {
-        this.heroDamage = heroDamage;
-    }
-
-    public int getHeroHealth() {
-        return heroHealth;
-    }
-
-    public void setHeroHealth(int heroHealth) {
-        this.heroHealth = heroHealth;
-    }
-
-    public WeaponType getHeroWeapon() {
-        return heroWeapon;
-    }
-
-    public void setHeroWeapon(WeaponType heroWeapon) {
-        this.heroWeapon = heroWeapon;
-    }
     public String info(){
         return "Boss health: " + this.health + " " + "Boss damage: " + this.damage + " " + "Boss weapon: " + this.weapon;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         Boss boss = new Boss();
-        boss.setBossHealth(400);
-        boss.setBossDamage(50);
-        boss.setBossWeapon(WeaponType.BULAWA);
+        boss.setHealth(400);
+        boss.setDamage(50);
+        boss.setWeapon(WeaponType.BULAWA);
 
         System.out.println(boss.info());
 
         Skeleton skeleton1 = new Skeleton();
-        skeleton1.setBossHealth(100);
-        skeleton1.setBossDamage(20);
-        skeleton1.setBossWeapon(WeaponType.BOW);
+        skeleton1.setHealth(100);
+        skeleton1.setDamage(20);
+        skeleton1.setWeapon(WeaponType.BOW);
         skeleton1.setArrows(10);
 
         System.out.println(skeleton1.info());
 
 
         Skeleton skeleton2 = new Skeleton();
-        skeleton2.setBossHealth(120);
-        skeleton2.setBossDamage(15);
-        skeleton2.setBossWeapon(WeaponType.BOW);
+        skeleton2.setHealth(120);
+        skeleton2.setDamage(15);
+        skeleton2.setWeapon(WeaponType.BOW);
         skeleton2.setArrows(15);
 
         System.out.println(skeleton2.info());
